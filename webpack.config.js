dee917e9d7035d2f29e333c0fa51d9330019be82
Andy Brown <andrewtcb@gmail.com>
2019-03-25 22:44:28 +1100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
@@ -35,16 +36,20 @@ module.exports = {
     },
     resolve: { extensions: ["*", ".js", ".jsx"] },
     output: {
-        path: path.resolve(__dirname, "dist/"),
-        publicPath: "/dist/",
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/",
         filename: "bundle.js"
     },
     devServer: {
-        contentBase: path.join(__dirname, "public/"),
+        contentBase: "./build",
         port: 3000,
-        publicPath: "http://localhost:3000/dist/",
+        publicPath: "http://localhost:3000/build/",
         hotOnly: true,
         historyApiFallback: true,
     },
-    plugins: [new webpack.HotModuleReplacementPlugin()]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve('./public/index.html'),
+        }),
+    ]
 };
